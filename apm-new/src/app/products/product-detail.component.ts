bc5d56d1aa5ffdcffbbd8a5104f6cae6d31399bd
David <j.david82@gmail.com>
2023-09-25 22:29:20 +1000

    Pass parameters to a route and handling null and undefined

--- apm-new/src/app/products/product-detail.component.ts
+++ apm-new/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail', // we are removing the selector because we don't want to embed it, instead we want to use routing to display it
@@ -7,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined; // that single pipe means "or", so it's either an IProduct or an undefined (because we have to wait for the heep response to populate this property)
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      // let's dummy up a product for now
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March, 19, 2021',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
+  }
 }
